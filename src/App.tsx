e15dc06a68e115163bc9356a2511633cd52bd91a
Krys-Salazar <krisachava@gmail.com>
2025-07-16 11:02:45 -0600

    Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import AboutUsPage from './pages/AboutUsPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <div>
+        <div className="min-h-screen bg-green-600 flex items-center justify-center">
+      <h1 className="text-black text-9xl font-bold"> Tailwind funciona en TSX </h1>
+    </div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        {/* <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
+      <AboutUsPage/>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function AboutUsPage() {
   return (
-    <div>AboutUsPage</div>
+    <div>
+      <div className="min-h-screen bg-green-600 flex items-center justify-center">
+        <h1 className="text-black text-9xl font-bold"> Holaaaaa </h1>
+      </div>
+    </div>
   )
 }
