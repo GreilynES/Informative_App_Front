359c9d824c928f2b653e32ae0f505f1c5f67b210
Krys-Salazar <krisachava@gmail.com>
2025-07-24 17:14:33 -0600

    Services.ts

---
+++ src/models/Services.ts
@@ -0,0 +1,28 @@
+export interface ModalContent {
+  title: string;
+  description: string;
+}
+
+  export const services = [
+    {
+      title: "Capacitación y transferencia",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      image:
+        "https://scontent.fsjo10-1.fna.fbcdn.net/v/t39.30808-6/482000334_571803125909150_7851598428227402418_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=01jFx4XBIMkQ7kNvwHAAqZS&_nc_oc=AdnkECuW7sVhMVDaM8Y0ujC3IKTJL_eksa8ErZVrix40ZkfjUW3JH2sOuQYD705jGOI&_nc_zt=23&_nc_ht=scontent.fsjo10-1.fna&_nc_gid=jxmr0kBgv9_wBt1imUay7w&oh=00_AfRUPDFZptbfTmmAk8uikLHtqMg0RBStKmnWs6_WY2QM4A&oe=687F373A",
+    },
+    {
+      title: "Suministro",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      image:
+        "https://scontent.fsjo10-1.fna.fbcdn.net/v/t39.30808-6/468735561_500353246387472_7430796887605064981_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=u8Kp6TsfmVIQ7kNvwHOanRA&_nc_oc=Adn3xnKfy-v5JbCOFzil8aEB_0g7Vw5mrkIuKVfjVQ_Ki7Oqxw4K2a5JutYj-gPF9Bc&_nc_zt=23&_nc_ht=scontent.fsjo10-1.fna&_nc_gid=Lm4N33vv_yeBXRKKEpbJkg&oh=00_AfRC4BHg9JPKlt6Wyz5p_gRutkvl02sx8wTq6BnICZ5e5w&oe=687F18F4",
+    },
+    {
+      title: "Centro de acopio de ganado",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      image:
+        "https://scontent.fsjo10-1.fna.fbcdn.net/v/t39.30808-6/480681953_559776447111818_994803168973652271_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=2C0C-HD-yqIQ7kNvwGVGsMm&_nc_oc=AdmW3WOYC7xD2T-swz9HyDq4oSyt7_hd2TRm5rg6xZhVkO_U-lXDTc-Jo8Qv-m3jG9E&_nc_zt=23&_nc_ht=scontent.fsjo10-1.fna&_nc_gid=m0INk5YFw5Lmtfxc8ujENw&oh=00_AfQhn5mcIhv5dHQ6mJl29bl9qtm8HAJdvclfzXOwEhS8dw&oe=687F4222",
+    },
+  ];
