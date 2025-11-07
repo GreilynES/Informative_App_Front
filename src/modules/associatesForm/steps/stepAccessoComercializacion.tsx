6a4afd5f823489d83584b554239e4ed8de209553
KaMendez04 <katherynmq@gmail.com>
2025-11-07 00:30:39 -0600

    Quitar mensajes innecesarios

--- src/modules/associatesForm/steps/stepAccessoComercializacion.tsx
+++ src/modules/associatesForm/steps/stepAccessoComercializacion.tsx
@@ -87,33 +87,6 @@ export function Step5({ form, onNext, onPrev }: Step5Props) {
 
   return (
     <div className="space-y-6">
-      {/* Banner de error general */}
-      {intentado && disableNext && (
-        <div className="bg-red-50 border-l-4 border-red-500 p-4 rounded">
-          <div className="flex items-start">
-            <svg className="w-5 h-5 text-red-500 mt-0.5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293z" clipRule="evenodd" />
-            </svg>
-            <div>
-              <h3 className="text-sm font-semibold text-red-800">
-                Complete los siguientes campos obligatorios:
-              </h3>
-              <ul className="text-sm text-red-700 mt-2 list-disc list-inside space-y-1">
-                {faltan.accesos && (
-                  <li>Debe seleccionar o agregar al menos una vía de acceso</li>
-                )}
-                {faltan.canales && (
-                  <li>Debe seleccionar o agregar al menos un canal de comercialización</li>
-                )}
-                {faltan.necesidades && (
-                  <li>Debe ingresar al menos una necesidad o mejora</li>
-                )}
-              </ul>
-            </div>
-          </div>
-        </div>
-      )}
-
       {/* Secciones con refs y scroll-margin */}
       <div ref={accesoRef} className="scroll-mt-24">
         <AccesoSection form={form} showErrors={intentado} />
--- src/modules/associatesForm/steps/stepPersonalInformation.tsx
+++ src/modules/associatesForm/steps/stepPersonalInformation.tsx
@@ -186,24 +186,6 @@ export function Step1({ form, lookup, onNext, canProceed }: Step1Props) {
 
   return (
     <div className="space-y-6">
-      {intentoAvanzar && Object.keys(erroresValidacion).length > 0 && (
-        <div className="bg-red-50 border-l-4 border-red-500 p-4 rounded">
-          <div className="flex items-start">
-            <svg className="w-5 h-5 text-red-500 mt-0.5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293z" clipRule="evenodd" />
-            </svg>
-            <div>
-              <h3 className="text-sm font-semibold text-red-800">
-                Por favor complete los campos obligatorios
-              </h3>
-              <p className="text-sm text-red-700 mt-1">
-                Hay {Object.keys(erroresValidacion).length} campo(s) que requieren su atención.
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
-
       {/* Información Personal */}
       <div className="bg-[#FAF9F5] rounded-xl shadow-md border border-[#DCD6C9]">
         <div className="px-6 py-4 border-b border-[#DCD6C9] flex items-center space-x-2">
